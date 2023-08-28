ddff359006928fa200a45ab07ba1c695b62eb555
vlad_vosov <vlad.shvetesov@gmail.com>
2023-08-28 21:00:55 +1000

    third

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,10 @@ def index(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} тел: {phone}, {message}')
+
     return render(request, 'catalog/contacts.html')
