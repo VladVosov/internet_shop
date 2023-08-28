789098c39278c56f22e3063fe7d16f72588a08bc
vlad_vosov <vlad.shvetesov@gmail.com>
2023-08-28 22:29:36 +1000

    third.1

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['BASE DIR / "catalog/templates"'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
